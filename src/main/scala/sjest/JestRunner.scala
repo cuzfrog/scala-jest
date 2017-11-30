f4eb852a83a487f76bf7e90dcc63c483b6cc2808
Cause Cheng <cuzfrog@139.com>
2017-11-30 16:51:09 +0800

    Remove done flag

--- src/main/scala/sjest/JestRunner.scala
+++ src/main/scala/sjest/JestRunner.scala
@@ -7,17 +7,11 @@ private class JestRunner(override val args: Array[String],
                          testClassLoader: ClassLoader)
                         (implicit config: TestFrameworkConfig) extends sbt.testing.Runner {
 
-  private var isDone: Boolean = false
-
   override def tasks(taskDefs: Array[TaskDef]): Array[Task] =
     taskDefs.map(new JestTask(_, testClassLoader))
 
   override def done(): String = {
-    if (isDone) throw new IllegalStateException("The runner has already been executed.")
-    else {
-      this.isDone = true
-      "my-test completed."
-    }
+    "my-test completed."
   }
 
   override def receiveMessage(msg: String): Option[String] = {
---
+++ tests/src/test/scala/anywhere/Test2.scala
@@ -0,0 +1,9 @@
+package anywhere
+
+import sjest.JestSuite
+
+object Test2 extends JestSuite {
+  test("this is my second test") {
+    println("do some test2!")
+  }
+}
