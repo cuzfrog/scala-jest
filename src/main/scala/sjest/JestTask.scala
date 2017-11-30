a96f94fbd42984a57b811791bb09db58803b0363
Cause Chung <cuzfrog@139.com>
2017-11-30 21:39:29 +0800

    Extract nodejs test into NodejsTestImpl

--- src/main/scala/sjest/JestTask.scala
+++ src/main/scala/sjest/JestTask.scala
@@ -1,11 +1,7 @@
 package sjest
 
-import nodejs.ChildProcess
 import org.scalajs.testinterface.TestUtils
 import sbt.testing._
-
-import scala.concurrent.duration.Deadline
-import scala.util.control.NonFatal
 
 private class JestTask(override val taskDef: TaskDef,
                        testClassLoader: ClassLoader)
@@ -23,7 +19,7 @@ private class JestTask(override val taskDef: TaskDef,
     val jsTestPath = JsTestConverter.generateJsTest(jsTestCase)
 
     val resultEvent =
-      if (config.autoRunTestInSbt) runNpmTest(jsTestPath, loggers)
+      if (config.autoRunTestInSbt) NodejsTestImpl.runTest(jsTestPath, loggers)
       else {
         loggers.foreach(_.info("---jest--- test.js files are generated," +
           " manually run the tests because auto run has been disabled"))
@@ -38,33 +34,5 @@ private class JestTask(override val taskDef: TaskDef,
                        continuation: Array[Task] => Unit): Unit = {
     this.execute(eventHandler, loggers)
     continuation(Array.empty)
-  }
-
-  private def runNpmTest(jsTestPath: String,
-                         loggers: Array[Logger])(implicit taskDef: TaskDef): Event = {
-    val startTime = Deadline.now
-    val event = try {
-      loggers.foreach(_.info(s"---jest--- Testing against: ${taskDef.fullyQualifiedName}"))
-      val JestFramework.NodejsCmd(cmd, args) = config.npmCmdOfPath(jsTestPath)
-      val childProcess = ChildProcess.spawnSync(cmd, args) //run code with nodejs
-      //loggers.foreach(_.info(childProcess.toString))
-      childProcess.status match {
-        case 0 =>
-          val output = Option(childProcess.output)
-          output.foreach(out => loggers.foreach(_.info(out.toString)))
-          loggers.foreach(_.info(s"PASS ${taskDef.fullyQualifiedName}"))
-          JestTestEvent(Status.Success)
-        case _ =>
-          loggers.foreach(_.error(s"test failed: "))
-          JestTestEvent(Status.Failure)
-      }
-    } catch {
-      case NonFatal(t) =>
-        loggers.foreach(_.error(s"test failed with $t}"))
-        JestTestEvent(Status.Failure)
-    }
-
-    val duration = (Deadline.now - startTime).toMillis
-    event.copy(duration = duration)
   }
 }
---
+++ src/main/scala/sjest/NodejsTestImpl.scala
@@ -0,0 +1,40 @@
+package sjest
+
+import sbt.testing.{Event, Logger, Status, TaskDef}
+import sjest.nodejs.ChildProcess
+
+import scala.concurrent.duration.Deadline
+import scala.util.control.NonFatal
+
+private object NodejsTestImpl {
+
+  def runTest(jsTestPath: String, loggers: Array[Logger])
+             (implicit taskDef: TaskDef, config: TestFrameworkConfig): Event = {
+    val startTime = Deadline.now
+    val event = try {
+      loggers.foreach(_.info(s"---jest--- Testing against: ${taskDef.fullyQualifiedName}"))
+      val JestFramework.NodejsCmd(cmd, args) = config.npmCmdOfPath(jsTestPath)
+      val childProcess = ChildProcess.spawnSync(cmd, args) //run code with nodejs
+      //loggers.foreach(_.info(childProcess.toString))
+      childProcess.status match {
+        case 0 =>
+          val output = Option(childProcess.output)
+          output.foreach(out => loggers.foreach(_.info(out.toString)))
+          loggers.foreach(_.info(s"PASS ${taskDef.fullyQualifiedName}"))
+          JestTestEvent(Status.Success)
+        case _ =>
+          loggers.foreach(_.error(s"test failed: "))
+          JestTestEvent(Status.Failure)
+      }
+    } catch {
+      case NonFatal(t) =>
+        loggers.foreach(_.error(s"test failed with $t}"))
+        JestTestEvent(Status.Failure)
+    }
+
+    val duration = (Deadline.now - startTime).toMillis
+    event.copy(duration = duration)
+  }
+
+
+}
