ee9a1f1c5efe7b0e2250792ca8ecb814a43217e9
Cause Chung <cuzfrog@139.com>
2017-12-07 13:56:38 +0800

    Refactor and test JsTestStub adding describe group support

--- src/main/scala/sjest/impl/JsTestStub.scala
+++ src/main/scala/sjest/impl/JsTestStub.scala
@@ -8,13 +8,19 @@ import scala.scalajs.reflect.Reflect
 
 private object JsTestStub {
   @JSExportTopLevel("loadTest")
-  def loadTest(fqcn: String, testName: String): js.Function = {
+  def loadTest(fqcn: String, descr: js.UndefOr[String], testName: String): js.Function = {
     val testSuite = Reflect.lookupInstantiatableClass(fqcn) match {
       case Some(moduleClass) => moduleClass.newInstance().asInstanceOf[JestSuite]
       case None => throw new ClassNotFoundException(fqcn)
     }
+    val descrOpt = descr.toOption
+    val group = testSuite.jsTestContainer.getGroups.find(_.descr == descrOpt) match {
+      case Some(g) => g
+      case None => throw new IllegalArgumentException(
+        s"Cannot find describe group '$descr' in '$fqcn', is *opt.js file stale?")
+    }
 
-    testSuite.jsTestContainer.getTests.find(_.name == testName) match {
+    group.getTests.find(_.name == testName) match {
       case Some(test) => test.runBlock
       case None => throw new IllegalArgumentException(
         s"Cannot find test '$testName' in '$fqcn', is *opt.js file stale?")
--- src/test/scala/sjest/impl/JsTestStubTest.scala
+++ src/test/scala/sjest/impl/JsTestStubTest.scala
@@ -12,20 +12,22 @@ object JsTestStubTest extends TestSuite {
 
   val tests = Tests {
     "loadTest-positive" - {
-      val function =
-        JsTestStub.loadTest(classname, "mock test").asInstanceOf[js.Function0[String]]
+      val function = JsTestStub.loadTest(classname, js.undefined, "mock test").asInstanceOf[js.Function0[String]]
       assert(sideEffectMarker == 0)
       function()
       assert(sideEffectMarker == 1)
     }
     "loadTest-negative-classname" - {
       intercept[ClassNotFoundException] {
-        JsTestStub.loadTest("some.bad.name", "")
+        JsTestStub.loadTest("some.bad.name", js.undefined, "mock test")
       }
     }
-    "loadTest-negative-description" - {
+    "loadTest-negative-bad-id" - {
       intercept[IllegalArgumentException] {
-        JsTestStub.loadTest(classname, "bad description")
+        JsTestStub.loadTest(classname, "bad descr", "mock test")
+      }
+      intercept[IllegalArgumentException] {
+        JsTestStub.loadTest(classname, js.undefined, "bad test name")
       }
     }
   }
